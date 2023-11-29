e2c2a7dccc1ff1ea0016c5a063c3a7af76bc4dbb
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-11-29 20:40:50 +0100

    Confirm deletion

--- JS/Modules/deleteOrder.js
+++ JS/Modules/deleteOrder.js
@@ -1,18 +1,29 @@
-import { sendObjectAsJson } from "./sendObjectAsJson.js";
+import {sendObjectAsJson} from "./sendObjectAsJson.js";
 
-export async function deleteOrder(orderIdToDelete) {
+export async function deleteOrder(orderIdToDelete)
+{
     const url = `http://localhost:8080/deleteOrder/${orderIdToDelete}`;
 
     console.log("Deleting Order with ID: ", orderIdToDelete); // Debugging line
 
-    try {
-        const response = await sendObjectAsJson(url, null, "DELETE");
-        if (response.status === 200) {
-            alert("Order deleted successfully");
-        } else {
-            alert("Failed to delete order. Please try again.");
+    try
+    {
+        if (confirm("Are you sure you want to delete the order with id: " + orderIdToDelete + "?"))
+        {
+            const response = await sendObjectAsJson(url, null, "DELETE");
+            if (response.status === 200)
+            {
+                alert("Order deleted successfully");
+            }
+            else
+            {
+                alert("Failed to delete order. Please try again.");
+            }
         }
-    } catch (error) {
+
+    }
+    catch (error)
+    {
         console.error(error);
         alert("An error occurred while deleting the order.");
     }
