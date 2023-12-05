3465d88d7ca49eea11ae45ac09d4281314fbbf8a
Frederik Wessel <frederikglimsager@hotmail.com>
2023-12-05 11:16:38 +0100

    dropdown virker, copy buttons virker ikke

--- JS/Modules/populateOrderTable.js
+++ JS/Modules/populateOrderTable.js
@@ -11,14 +11,30 @@ export function populateTable(orders)
         const productNameCell = row.insertCell();
         productNameCell.textContent = order.productName;
 
+        // const shippingCell = row.insertCell();
+        // const shippingButton = document.createElement('button');
+        // shippingButton.textContent = 'Shipping';
+        // shippingButton.id = 'btnShipping';
+        // shippingButton.addEventListener('click', () => {
+        //     window.location.href = `shippingOverview.html?orderId=${order.orderId}`;
+        // });
+        // shippingCell.appendChild(shippingButton);
+
         const shippingCell = row.insertCell();
         const shippingButton = document.createElement('button');
         shippingButton.textContent = 'Shipping';
         shippingButton.id = 'btnShipping';
-        shippingButton.addEventListener('click', () => {
-            window.location.href = `shippingOverview.html?orderId=${order.orderId}`;
-        });
+        shippingButton.classList.add('btnShipping'); // use class instead of id for multiple elements
         shippingCell.appendChild(shippingButton);
+
+        const shippingDetailsDropdown = document.createElement('div');
+        shippingDetailsDropdown.classList.add('shipping-details-dropdown', 'hidden');
+        shippingDetailsDropdown.id = `shippingDetails${order.orderId}`; // unique id for each dropdown
+        row.insertAdjacentElement('afterend', shippingDetailsDropdown); // insert the dropdown after the row
+
+        shippingButton.addEventListener('click', () => {
+            fetchAndDisplayShippingDetails(order.orderId, shippingDetailsDropdown);
+        });
 
         const priceCell = row.insertCell();
         priceCell.textContent = order.price;
@@ -43,3 +59,32 @@ export function populateTable(orders)
         actionCell.appendChild(deleteButton);
     });
 }
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+        // Optional: Display a message or change the button appearance to indicate that the text has been copied
+        alert('Copied to clipboard: ' + text); // Simple alert; you might want to use a more subtle notification
+    }).catch(err => {
+        console.error('Failed to copy text to clipboard', err);
+        // Optional: Display an error message
+    });
+}
+
+async function fetchAndDisplayShippingDetails(orderId, dropdownElement) {
+    try {
+        const response = await fetch(`http://localhost:8080/getCustomerAddressByOrderId/${orderId}`);
+        const details = await response.json();
+        dropdownElement.innerHTML = `
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.costumerName}')">Copy</button> Name: <span>${details.costumerName}</span></div>
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.streetAddress}')">Copy</button> Address: <span>${details.streetAddress}</span></div>
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.postalCode}')">Copy</button> Postal Code: <span>${details.postalCode}</span></div>
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.city}')">Copy</button> City: <span>${details.city}</span></div>
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.country.countryName}')">Copy</button> Country: <span>${details.country.countryName}</span></div>
+            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.phone}')">Copy</button> Phone: <span>${details.phone}</span></div>
+        `;
+        dropdownElement.classList.toggle('hidden'); // Show/hide the dropdown
+    } catch (error) {
+        console.error(error);
+    }
+}
+
