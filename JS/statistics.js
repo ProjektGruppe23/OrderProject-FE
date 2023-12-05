828b0a4ad89940d4ed7aa5bdc8fc0dca37d8a857
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-12-05 13:22:18 +0100

    make test method

--- JS/statistics.js
+++ JS/statistics.js
@@ -1,3 +1,4 @@
+import { totalOrders } from './modules/statistics/totalOrders.js';
 
 
 
@@ -6,3 +7,16 @@ document.addEventListener('DOMContentLoaded', async () =>
 {
     await totalOrders();
 });
+
+function incomeByProduct()
+{
+    let incomeByProduct = 0;
+    for (let i = 0; i < getAllData.length; i++)
+    {
+        incomeByProduct += getAllData[i].price;
+        if(getAllData[i].productName === 'Sweater')
+        {
+            incomeByProduct += getAllData[i].price;
+        }
+    }
+}
