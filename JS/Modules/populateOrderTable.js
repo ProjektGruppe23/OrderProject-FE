1525382397cbe1d6c3a8daa6acb517c62b7e37e8
Frederik Wessel <frederikglimsager@hotmail.com>
2023-12-06 10:03:27 +0100

    removed javascript from html

--- JS/Modules/populateOrderTable.js
+++ JS/Modules/populateOrderTable.js
@@ -1,4 +1,7 @@
+import { copyToClipboard } from './copyToClipboard.js';
+window.copyToClipboard = copyToClipboard;
 import { deleteOrder } from "./deleteOrder.js";
+import { fetchAndDisplayShippingDetails } from "./fetchShippingDetails.js";
 
 export function populateTable(orders)
 {
@@ -10,15 +13,6 @@ export function populateTable(orders)
 
         const productNameCell = row.insertCell();
         productNameCell.textContent = order.productName;
-
-        // const shippingCell = row.insertCell();
-        // const shippingButton = document.createElement('button');
-        // shippingButton.textContent = 'Shipping';
-        // shippingButton.id = 'btnShipping';
-        // shippingButton.addEventListener('click', () => {
-        //     window.location.href = `shippingOverview.html?orderId=${order.orderId}`;
-        // });
-        // shippingCell.appendChild(shippingButton);
 
         const shippingCell = row.insertCell();
         const shippingButton = document.createElement('button');
@@ -62,21 +56,5 @@ export function populateTable(orders)
 
 
 
-async function fetchAndDisplayShippingDetails(orderId, dropdownElement) {
-    try {
-        const response = await fetch(`http://localhost:8080/getCustomerAddressByOrderId/${orderId}`);
-        const details = await response.json();
-        dropdownElement.innerHTML = `
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.costumerName}')">Copy</button> Name: <span>${details.costumerName}</span></div>
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.streetAddress}')">Copy</button> Address: <span>${details.streetAddress}</span></div>
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.postalCode}')">Copy</button> Postal Code: <span>${details.postalCode}</span></div>
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.city}')">Copy</button> City: <span>${details.city}</span></div>
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.country.countryName}')">Copy</button> Country: <span>${details.country.countryName}</span></div>
-            <div class="detail"><button class="copy-btn" onclick="copyToClipboard('${details.phone}')">Copy</button> Phone: <span>${details.phone}</span></div>
-        `;
-        dropdownElement.classList.toggle('hidden'); // Show/hide the dropdown
-    } catch (error) {
-        console.error(error);
-    }
-}
+
 
