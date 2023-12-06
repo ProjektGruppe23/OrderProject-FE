150f23a91272a3fd0207f1792886cf9a15f38e81
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-12-06 23:38:04 +0100

    alert for data fetching statistics

--- JS/statistics/modules/fetchAnalyticsData.js
+++ JS/statistics/modules/fetchAnalyticsData.js
@@ -5,10 +5,12 @@ export async function fetchAnalyticsData()
 {
     const url = 'http://localhost:8080/getinfotoanalyze';
     try {
+        alert("please wait for data to be fetched, click ok to continue");
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
+        alert("data has been fetched, please click ok to resume");
         analyticsData = await response.json();
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
