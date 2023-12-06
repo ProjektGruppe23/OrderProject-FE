ef8e8426828211ad609d4ee1729f5f3cd6c1d7ac
Christian <107435301+PedroLorenzen@users.noreply.github.com>
2023-12-06 22:56:34 +0100

    css and only fetch data ones

--- JS/statistics/statistics.js
+++ JS/statistics/statistics.js
@@ -2,6 +2,7 @@ import { totalOrders } from './modules/totalOrders.js';
 import { populateTable } from "./modules/populateTableOrderCount.js";
 import { displayBarChart } from "./modules/ordersByCountry.js"
 import { displayDonutChart } from "./modules/ordersByVendor.js"
+import {fetchAnalyticsData} from "./modules/fetchAnalyticsData";
 
 /*async function getAnalyticsData()
 {
@@ -11,6 +12,8 @@ import { displayDonutChart } from "./modules/ordersByVendor.js"
 document.addEventListener('DOMContentLoaded',
     async () =>
     {
+        //await fetchAnalyticsData();
+
         const totalOrdersNumber = await totalOrders();
         console.log(totalOrdersNumber);
 
